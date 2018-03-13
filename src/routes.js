80a743ad359049efa188c9185253ead1a062058f
Chris Maltby <chris.maltby@gmail.com>
2018-03-13 19:44:42 +0000

    Scroll to top on page transition

--- src/routes.js
+++ src/routes.js
@@ -2,25 +2,27 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import PageTransition from "react-router-page-transition";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import App from "./App";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectPage from "./pages/ProjectPage";
 
-export default () => (
+export default () =>
   <Router>
-    <App>
-      <Route
-        render={({ location }) => (
-          <PageTransition timeout={1000}>
-            <Switch location={location}>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/about" component={AboutPage} />
-              <Route exact path="/projects/:id" component={ProjectPage} />
-            </Switch>
-          </PageTransition>
-        )}
-      />
-    </App>
-  </Router>
-);
+    <ScrollToTop>
+      <App>
+        <Route
+          render={({ location }) =>
+            <PageTransition timeout={1000}>
+              <Switch location={location}>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/about" component={AboutPage} />
+                <Route exact path="/projects/:id" component={ProjectPage} />
+              </Switch>
+            </PageTransition>}
+        />
+      </App>
+    </ScrollToTop>
+  </Router>;
